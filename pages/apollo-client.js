03ca10c8355609c6e6b56908adcfbaaeac0dace4
robmi10 <89600540+robmi10@users.noreply.github.com>
2024-02-14 21:19:37 +0100

    tweak

--- pages/apollo-client.js
+++ pages/apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  // uri: 'https://api.studio.thegraph.com/query/47164/equbfacotry/v0.0.25',
-  uri: 'http://127.0.0.1:8000/subgraphs/name/equbv50',
+  uri: 'https://api.studio.thegraph.com/query/47164/equbfacotry/v0.0.27',
+  // uri: 'http://127.0.0.1:8000/subgraphs/name/equbv50',
   cache: new InMemoryCache(),
 });
